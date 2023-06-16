e8819eb03abefa8034c51c01bbdfaf58e40c0b66
andrey <larginator@gmail.com>
2023-06-17 02:41:41 +0600

    💾 Add ProductCategory and Product models

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -13,26 +14,8 @@ def index(request: HttpRequest) -> HttpResponse:
 def products(request: HttpRequest) -> HttpResponse:
     context = {
         'title': 'Store - Каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': 23725,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390,
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-            },
-        ],
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     }
 
     return render(request, 'products/products.html', context=context)
